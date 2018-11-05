26e3337fa11fc77708cc04941ee06550a3105bdb
Yan <yanvictor_ds@hotmail.com>
2018-11-04 22:55:46 -0200

    Update.

--- Prototipo_Redes/TG2/API_Filter/scripts/clean.sh
+++ Prototipo_Redes/TG2/API_Filter/scripts/clean.sh
@@ -1,1 +1,5 @@
-rm -f ../src/*/*.o && rm -f ../test/u && rm -f ../test/r && rm -f ../blacklist.txt && rm -f ../api
+find ../src -name "*.o" -type f -print0 | xargs -0 /bin/rm -f
+rm -f ../test/u 
+rm -f ../test/r 
+rm -f ../blacklist.txt 
+rm -f ../api
--- Prototipo_Redes/TG2/API_Filter/scripts/run.sh
+++ Prototipo_Redes/TG2/API_Filter/scripts/run.sh
@@ -1,2 +1,2 @@
-make
-../api
+make --directory ../
+./../api
